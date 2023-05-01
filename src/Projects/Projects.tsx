65f0f1d1942560c56ea5bf6fd938eee9760f6900
Ari Wisenburn <ari.wisenburn@gmail.com>
2023-05-01 14:16:43 -0400

    feat(page): Add flex boxes to projects page

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -1,16 +1,17 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text, VStack } from '@chakra-ui/react'
 
 export default function Projects(props: any) {
   return (
-    <Flex wrap='wrap' bg='isabelline' position='relative' h='100%' alignContent={'center'} fontSize='32px' p='256px'>
-      <Text >
-        Right now, my main project is this website.
-        Check out my github and Linkedin in the footer for more projects I've done in the past,
-        and check back here soon for more progress!
-      </Text>
-      <Text pt='64px'>
-        FYI: there's a secret surprise on this page (well, every page) - see if you can find it ;)
-      </Text>
-    </Flex>
+    <Flex direction='column'>
+      <Flex direction={['column', 'column', 'row']}>
+        <Box bg='tomato' w='400px' h='400px' />
+        <Box bg='orange' w='400px' h='400px' />
+      </Flex>
+
+      <Flex direction={['column', 'column', 'row']}>
+        <Box bg='sage' w='400px' h='400px' />
+        <Box bg='cornflowerblue' w='400px' h='400px' />
+      </Flex>
+    </Flex >
   )
 }
